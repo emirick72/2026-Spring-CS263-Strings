88d13cd8265fd741ed2e9b4f00d03df8f5f73bbf
Emily Rick <emrick.2261@gmail.com>
2026-02-09 19:13:18 -0500

    Finished Asst. 2 (Strings assignment)

--- examine_sentence.py
+++ examine_sentence.py
@@ -2,23 +2,62 @@
 
 def first_letter_is_capitalized(sentence: str) -> bool:
     """
-    T.B.W.
-    """
+    Check that the first letter of a sentence is capitalized.
 
-    return None
+    Args:
+        sentence: The user-inputted string that gets checked.
+
+    Returns:
+        True if the first letter of the sentence is capitalized, False if otherwise.
+    """
+    
+    if not sentence:
+        return False
+    
+    if sentence[0].isupper():
+        return True
+    else:
+        return False
 
 
 def ends_with_punctuation(sentence: str) -> bool:
     """
-    T.B.W.
+    Check that a sentence ends with a period, question mark, or exclamation point.
+
+    Args:
+        sentence: The user-inputted string that gets checked.
+
+    Returns:
+        True if the sentence ends with a period, question mark, or an exclamation point,
+        False if otherwise.
     """
 
-    return None
+    valid_punctuation = {'.', '?', '!'}
+    if sentence and sentence[-1] in valid_punctuation:
+        return True
+    else:
+        return False
+
 
 
 def includes_advanced_punctuation(sentence: str) -> bool:
     """
-    T.B.W.
+    Check for quotes, commas, semicolons, colons, dashes, and ellipses.
+
+    Args:
+        sentence: The user-inputted string that gets checked.
+
+    Returns:
+        True if the sentence has quotes, commas, semicolons, colons, dashes, and ellipses,
+        False if otherwise.
     """
 
-    return None
+    valid_advanced_punctuation = {'""', ',', ';', ':', '-', '...'}
+    for char in sentence:
+        if char in valid_advanced_punctuation:
+            return True
+    
+    # Handle ellipses separately
+    if "..." in sentence:
+        return True
+    return False
